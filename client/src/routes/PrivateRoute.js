1479095821bdd2d1ef9622ce61955d46fb20ef1e
vishal vilas kamble <vishalvkamble95@gmail.com>
2024-09-04 21:35:19 +0530

    minor change

--- client/src/routes/PrivateRoute.js
+++ client/src/routes/PrivateRoute.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Outlet, Navigate } from "react-router-dom";
-import jwt_decode from "jwt-decode";
+
 const PrivateRoute = () => {
   const token = localStorage.getItem("token");
-  const data = token !== null && jwt_decode(token) && token !== undefined;
+  const data = token !== null && token !== undefined;
 
   return data ? <Outlet /> : <Navigate to="/login" />;
 };
